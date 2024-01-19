e73a7d9fbe4c57d845e32652ab338b368df65e94
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-01-18 19:46:24 -0500

    Update fllama.cpp

--- src/fllama.cpp
+++ src/fllama.cpp
@@ -1,13 +1,4 @@
 #include "fllama.h"
-
-// A very short-lived native function.
-//
-// For very short-lived functions, it is fine to call them on the main isolate.
-// They will block the Dart execution while running the native function, so
-// only do this for native functions which are guaranteed to be short-lived.
-FFI_PLUGIN_EXPORT intptr_t sum(intptr_t a, intptr_t b) { return a + b; }
-
-#include "fllama.h"
 #include "llama.h"
 #include "ggml.h"
 #include "common.h"
