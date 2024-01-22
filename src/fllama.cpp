a9c587a50188b2f3b4208c3237beb00d3f0b54a3
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-01-21 19:47:29 -0500

    clang-format

--- src/fllama.cpp
+++ src/fllama.cpp
@@ -100,7 +100,7 @@ static int n_past;
 static int n_consumed;
 
 void _fllama_inference_sync(fllama_inference_request request,
-                 fllama_inference_callback callback);
+                            fllama_inference_callback callback);
 // Current implementation based on llama.cpp/examples/simple/simple.cpp combined
 // with handling sampling manually via top_p and temp functions.
 //
@@ -153,157 +153,155 @@ fllama_inference(fllama_inference_request request,
 }
 
 void _fllama_inference_sync(fllama_inference_request request,
-                 fllama_inference_callback callback) {
-    std::cout << "[fllama] Inference thread started." << std::endl;
-    gpt_params params;
-    std::cout << "[fllama] Initializing params." << std::endl;
-    params.n_ctx = request.context_size;
-    std::cout << "[fllama] Context size: " << params.n_ctx << std::endl;
-    // Very unclear what this means, but, if its < the total number of tokens,
-    // llama.cpp assertion fails. (n_tokens <= n_batch)
-    params.n_batch = request.context_size;
-    std::cout << "[fllama] Batch size: " << params.n_batch << std::endl;
-    params.n_predict = request.max_tokens;
-    std::cout << "[fllama] Max tokens: " << params.n_predict << std::endl;
-    params.sparams.temp = request.temperature;
-    std::cout << "[fllama] Temperature: " << params.sparams.temp << std::endl;
-    params.sparams.samplers_sequence = "pt";
-    params.sparams.top_p = request.top_p;
-    std::cout << "[fllama] Top P: " << params.sparams.top_p << std::endl;
-    params.model = request.model_path;
-    std::cout << "[fllama] Model path: " << params.model << std::endl;
+                            fllama_inference_callback callback) {
+  std::cout << "[fllama] Inference thread started." << std::endl;
+  gpt_params params;
+  std::cout << "[fllama] Initializing params." << std::endl;
+  params.n_ctx = request.context_size;
+  std::cout << "[fllama] Context size: " << params.n_ctx << std::endl;
+  // Very unclear what this means, but, if its < the total number of tokens,
+  // llama.cpp assertion fails. (n_tokens <= n_batch)
+  params.n_batch = request.context_size;
+  std::cout << "[fllama] Batch size: " << params.n_batch << std::endl;
+  params.n_predict = request.max_tokens;
+  std::cout << "[fllama] Max tokens: " << params.n_predict << std::endl;
+  params.sparams.temp = request.temperature;
+  std::cout << "[fllama] Temperature: " << params.sparams.temp << std::endl;
+  params.sparams.samplers_sequence = "pt";
+  params.sparams.top_p = request.top_p;
+  std::cout << "[fllama] Top P: " << params.sparams.top_p << std::endl;
+  params.model = request.model_path;
+  std::cout << "[fllama] Model path: " << params.model << std::endl;
 // Force CPU if iOS simulator: no GPU support available, hangs.
 #if TARGET_IPHONE_SIMULATOR
-    model_params.n_gpu_layers = 0;
+  model_params.n_gpu_layers = 0;
 // Otherwise, for physical iOS devices and other platforms
 #else
-    params.n_gpu_layers = request.num_gpu_layers;
+  params.n_gpu_layers = request.num_gpu_layers;
 #endif
-    const char *metal_path = request.ggml_metal_path;
-    if (metal_path != nullptr) {
-      std::cout << "[fllama] Metal path: " << metal_path << std::endl;
-      setenv("GGML_METAL_PATH_RESOURCES", metal_path, 1);
-    } else {
-      std::cout << "[fllama] Metal path not provided; Metal not enabled. You "
-                   "should try to provide a path on macOS or iOS."
-                << std::endl;
-    }
-
-    llama_backend_init(params.numa);
-    std::tie(model, ctx) = llama_init_from_gpt_params(params);
-    if (model == NULL || ctx == NULL) {
-      std::cout << "[fllama] Unable to load model." << std::endl;
-      if (model != NULL) {
-        llama_free_model(model);
-      }
-      throw std::runtime_error("[fllama] Unable to initialize model");
-    }
-
-    std::vector<llama_token> tokens_list;
-    tokens_list = ::llama_tokenize(model, request.input, true);
-    std::cout << "[fllama] Input token count: " << tokens_list.size()
+  const char *metal_path = request.ggml_metal_path;
+  if (metal_path != nullptr) {
+    std::cout << "[fllama] Metal path: " << metal_path << std::endl;
+    setenv("GGML_METAL_PATH_RESOURCES", metal_path, 1);
+  } else {
+    std::cout << "[fllama] Metal path not provided; Metal not enabled. You "
+                 "should try to provide a path on macOS or iOS."
               << std::endl;
-    std::cout << "[fllama] Output tokens requested: " << params.n_predict
-              << std::endl;
-    const int n_max_tokens = request.max_tokens;
-    llama_context_params ctx_params =
-        llama_context_params_from_gpt_params(params);
-    std::cout << "Number of threads: " << ctx_params.n_threads << std::endl;
+  }
 
-    llama_batch batch =
-        llama_batch_init(tokens_list.size() + n_max_tokens, 0, 1);
-    // evaluate the initial prompt
-    for (size_t i = 0; i < tokens_list.size(); i++) {
-      llama_batch_add(batch, tokens_list[i], i, {0}, false);
+  llama_backend_init(params.numa);
+  std::tie(model, ctx) = llama_init_from_gpt_params(params);
+  if (model == NULL || ctx == NULL) {
+    std::cout << "[fllama] Unable to load model." << std::endl;
+    if (model != NULL) {
+      llama_free_model(model);
     }
-    // llama_decode will output logits only for the last token of the prompt
-    batch.logits[batch.n_tokens - 1] = true;
-    if (llama_decode(ctx, batch) != 0) {
-      LOG_TEE("%s: llama_decode() failed\n", __func__);
-      // throw runtime error
-      throw std::runtime_error("[fllama] llama_decode() failed");
-    }
+    throw std::runtime_error("[fllama] Unable to initialize model");
+  }
 
-    // main loop
-    int n_cur = batch.n_tokens;
-    int n_gen = 0;
-    int n_decode = 0;
-    std::string result;
+  std::vector<llama_token> tokens_list;
+  tokens_list = ::llama_tokenize(model, request.input, true);
+  std::cout << "[fllama] Input token count: " << tokens_list.size()
+            << std::endl;
+  std::cout << "[fllama] Output tokens requested: " << params.n_predict
+            << std::endl;
+  const int n_max_tokens = request.max_tokens;
+  llama_context_params ctx_params =
+      llama_context_params_from_gpt_params(params);
+  std::cout << "Number of threads: " << ctx_params.n_threads << std::endl;
 
-    const auto t_main_start = ggml_time_us();
+  llama_batch batch = llama_batch_init(tokens_list.size() + n_max_tokens, 0, 1);
+  // evaluate the initial prompt
+  for (size_t i = 0; i < tokens_list.size(); i++) {
+    llama_batch_add(batch, tokens_list[i], i, {0}, false);
+  }
+  // llama_decode will output logits only for the last token of the prompt
+  batch.logits[batch.n_tokens - 1] = true;
+  if (llama_decode(ctx, batch) != 0) {
+    LOG_TEE("%s: llama_decode() failed\n", __func__);
+    // throw runtime error
+    throw std::runtime_error("[fllama] llama_decode() failed");
+  }
 
-    while (n_gen <= n_max_tokens) {
-      {
-        auto n_vocab = llama_n_vocab(model);
-        auto *logits = llama_get_logits_ith(ctx, batch.n_tokens - 1);
+  // main loop
+  int n_cur = batch.n_tokens;
+  int n_gen = 0;
+  int n_decode = 0;
+  std::string result;
 
-        std::vector<llama_token_data> candidates;
-        candidates.reserve(n_vocab);
+  const auto t_main_start = ggml_time_us();
 
-        for (llama_token token_id = 0; token_id < n_vocab; token_id++) {
-          candidates.emplace_back(
-              llama_token_data{token_id, logits[token_id], 0.0f});
-        }
-        llama_token_data_array candidates_p = {candidates.data(),
-                                               candidates.size(), false};
-        // Why if?
-        // - Observed get repeated [PAD] output from stable lm Zephyr 3B at 0
-        // temp.
-        // - Observed infinitesimally larger than 0 gets same output as when
-        // llama_sample_temp is commented out.
-        // - llama_sample_temp implementation indicates silent divide by 0 when
-        // temp == 0.
-        if (params.sparams.temp > 0.0f) {
-          llama_sample_temp(ctx, &candidates_p, params.sparams.temp);
-        }
-        // Only bother with top_p if it's not 1.0f (consider all tokens)
-        // > 0 condition added out of caution, not tested.
-        if (params.sparams.top_p < 1.0f && params.sparams.top_p > 0.0f) {
-          llama_sample_top_p(ctx, &candidates_p, params.sparams.top_p,
-                             1 /* min_keep */);
-        }
-        const llama_token new_token_id = llama_sample_token(ctx, &candidates_p);
+  while (n_gen <= n_max_tokens) {
+    {
+      auto n_vocab = llama_n_vocab(model);
+      auto *logits = llama_get_logits_ith(ctx, batch.n_tokens - 1);
 
-        // is it an end of stream?
-        if (new_token_id == llama_token_eos(model) || n_gen == n_max_tokens) {
-          break;
-        }
-        result += llama_token_to_piece(ctx, new_token_id);
-        char *c_result =
-            (char *)malloc(result.size() + 1); // +1 for the null terminator
-        if (c_result) { // Ensure malloc succeeded before using the pointer
-          std::strcpy(c_result, result.c_str());
-          callback(c_result);
-          // Assuming the callback or the caller now owns the resource and will
-          // free it.
-        }
+      std::vector<llama_token_data> candidates;
+      candidates.reserve(n_vocab);
 
-        // prepare the next batch
-        llama_batch_clear(batch);
+      for (llama_token token_id = 0; token_id < n_vocab; token_id++) {
+        candidates.emplace_back(
+            llama_token_data{token_id, logits[token_id], 0.0f});
+      }
+      llama_token_data_array candidates_p = {candidates.data(),
+                                             candidates.size(), false};
+      // Why if?
+      // - Observed get repeated [PAD] output from stable lm Zephyr 3B at 0
+      // temp.
+      // - Observed infinitesimally larger than 0 gets same output as when
+      // llama_sample_temp is commented out.
+      // - llama_sample_temp implementation indicates silent divide by 0 when
+      // temp == 0.
+      if (params.sparams.temp > 0.0f) {
+        llama_sample_temp(ctx, &candidates_p, params.sparams.temp);
+      }
+      // Only bother with top_p if it's not 1.0f (consider all tokens)
+      // > 0 condition added out of caution, not tested.
+      if (params.sparams.top_p < 1.0f && params.sparams.top_p > 0.0f) {
+        llama_sample_top_p(ctx, &candidates_p, params.sparams.top_p,
+                           1 /* min_keep */);
+      }
+      const llama_token new_token_id = llama_sample_token(ctx, &candidates_p);
 
-        // push this new token for next evaluation
-        llama_batch_add(batch, new_token_id, n_cur, {0}, true);
-
-        n_decode += 1;
-        n_gen += 1;
+      // is it an end of stream?
+      if (new_token_id == llama_token_eos(model) || n_gen == n_max_tokens) {
+        break;
+      }
+      result += llama_token_to_piece(ctx, new_token_id);
+      char *c_result =
+          (char *)malloc(result.size() + 1); // +1 for the null terminator
+      if (c_result) { // Ensure malloc succeeded before using the pointer
+        std::strcpy(c_result, result.c_str());
+        callback(c_result);
+        // Assuming the callback or the caller now owns the resource and will
+        // free it.
       }
 
-      n_cur += 1;
+      // prepare the next batch
+      llama_batch_clear(batch);
 
-      // evaluate the current batch with the transformer model
-      if (llama_decode(ctx, batch)) {
-        fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
-        throw std::runtime_error("Inference failed");
-      }
+      // push this new token for next evaluation
+      llama_batch_add(batch, new_token_id, n_cur, {0}, true);
+
+      n_decode += 1;
+      n_gen += 1;
     }
 
-    // Log finished
-    const auto t_main_end = ggml_time_us();
-    const auto t_main = t_main_end - t_main_start;
-    fprintf(stderr, "main loop: %f ms\n", t_main / 1000.0f);
-    LOG_TEE("%s: decoded %d tokens in %.2f s, speed: %.2f t/s\n", __func__,
-            n_decode, (t_main_end - t_main_start) / 1000000.0f,
-            n_decode / ((t_main_end - t_main_start) / 1000000.0f));
-    llama_print_timings(ctx);
+    n_cur += 1;
 
-                 }
+    // evaluate the current batch with the transformer model
+    if (llama_decode(ctx, batch)) {
+      fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
+      throw std::runtime_error("Inference failed");
+    }
+  }
+
+  // Log finished
+  const auto t_main_end = ggml_time_us();
+  const auto t_main = t_main_end - t_main_start;
+  fprintf(stderr, "main loop: %f ms\n", t_main / 1000.0f);
+  LOG_TEE("%s: decoded %d tokens in %.2f s, speed: %.2f t/s\n", __func__,
+          n_decode, (t_main_end - t_main_start) / 1000000.0f,
+          n_decode / ((t_main_end - t_main_start) / 1000000.0f));
+  llama_print_timings(ctx);
+}
