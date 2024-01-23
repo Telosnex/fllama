53fbc71f515a7a0234c05ae4b7c7cf505669ad52
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-01-23 10:43:56 -0500

    Add iOS as copied source
    See fllama.cpp for info on build

---
+++ ios/llama.cpp/common/CMakeFiles/build_info.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build_info.dir/build-info.cpp.o"
+  "CMakeFiles/build_info.dir/build-info.cpp.o.d"
+  "build-info.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build_info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ios/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 1923;
+char const *LLAMA_COMMIT = "9b75cb2";
+char const *LLAMA_COMPILER = "Apple clang version 14.0.3 (clang-1403.0.22.14.1)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin22.5.0";
