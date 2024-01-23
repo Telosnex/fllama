0a211323be4b5091b4ee40d39afe58c95cc230ce
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-01-23 10:44:52 -0500

    Add macOS as copied source
    See fllama.cpp for details on build strategy

---
+++ macos/llama.cpp/common/CMakeFiles/build_info.dir/build-info.cpp.o.d
@@ -0,0 +1,2 @@
+llama.cpp/common/CMakeFiles/build_info.dir/build-info.cpp.o: \
+  /Users/jamesoleary/dev/fllama/src/llama.cpp/common/build-info.cpp
---
+++ macos/llama.cpp/common/CMakeFiles/build_info.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for build_info.
+# This may be replaced when dependencies are built.
