dc06ad301bb15edfcd2ec68e86630a9c661709ce
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-01-29 20:35:26 -0500

    Add stub build-info.cpp
    After new drop:
    undef: _LLAMA_BUILD_NUMBER
    undef: _LLAMA_BUILD_TARGET
    undef: _LLAMA_COMMIT
    undef: _LLAMA_COMPILER
    Undefined symbols for architecture arm64:
      "_LLAMA_BUILD_NUMBER", referenced from:
          gpt_params_parse_ex(int, char**, gpt_params&) in fllama_lto.o
          dump_non_result_info_yaml(__sFILE*, gpt_params const&, llama_context const*, std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char>> const&, std::__1::vector<int, std::__1::allocator<int>> const&, char const*) in fllama_lto.o
      "_LLAMA_BUILD_TARGET", referenced from:
          gpt_params_parse_ex(int, char**, gpt_params&) in fllama_lto.o
      "_LLAMA_COMMIT", referenced from:
          gpt_params_parse_ex(int, char**, gpt_params&) in fllama_lto.o
          dump_non_result_info_yaml(__sFILE*, gpt_params const&, llama_context const*, std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char>> const&, std::__1::vector<int, std::__1::allocator<int>> const&, char const*) in fllama_lto.o
      "_LLAMA_COMPILER", referenced from:
          gpt_params_parse_ex(int, char**, gpt_params&) in fllama_lto.o
    
    Unclear why this was fine before - commit did delete build-info.cpp, but we also
    didn't manually write it.
    
    Perhaps they were copied from src/

---
+++ ios/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 4268;
+char const *LLAMA_COMMIT = "ceebbb5b21b971941b2533210b74bf359981006c";
+char const *LLAMA_COMPILER = "fllama";
+char const *LLAMA_BUILD_TARGET = "fllama";
