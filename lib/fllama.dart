22d3953df9ae4ed9ccf7f9b79cac8fbba4331c96
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-08-22 19:45:48 -0400

    Use dart.library.js_interop
    https://x.com/jezell/status/1821428940453695507

--- lib/fllama.dart
+++ lib/fllama.dart
@@ -1,5 +1,5 @@
 export 'fllama_unimplemented.dart'
-    if (dart.library.html) 'fllama_html.dart'
+    if (dart.library.js_interop) 'fllama_html.dart'
     if (dart.library.io) 'fllama_io.dart';
 export 'fllama_universal.dart';
 export 'misc/openai.dart';
