35a5d0b7f157a5d7ae8118dd5789c8f7550fbf36
James O'Leary <jpohhhh@gmail.com>
2025-07-26 15:24:43 -0400

    fix: update build-info.cpp so its avail for win/linux

--- macos/llama.cpp/common/build-info.cpp
+++ macos/llama.cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 4939;
-char const *LLAMA_COMMIT = "5cd800b0";
-char const *LLAMA_COMPILER = "Apple clang version 16.0.0 (clang-1600.0.26.6)";
-char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin24.2.0";
+int LLAMA_BUILD_NUMBER = 0;
+char const *LLAMA_COMMIT = "unknown";
+char const *LLAMA_COMPILER = "unknown";
+char const *LLAMA_BUILD_TARGET = "unknown";
